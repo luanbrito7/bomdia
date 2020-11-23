d0eebc1a1efa5e7516dea7fc4cfdf0f6437b6310
luanbrito7 <lab7@cin.ufpe.br>
2020-11-23 20:19:16 -0300

    phrases api not working: add hardcoded phrases

--- server/phrase.js
+++ server/phrase.js
@@ -2,13 +2,17 @@ const axios = require('axios')
 
 async function getPhrase({type}) {
     if (!typeParams.includes(type)) type = getRandomType()
-    let { data } = await axios.get("http://allugofrases.herokuapp.com/frases/random")
-    let phrase = data.frase + getComplement(type)
+    // let { data } = await axios.get("http://allugofrases.herokuapp.com/frases/random") api not working
+    let phrase = getRandomPhrase() + getComplement(type)
     return phrase
 }
 
 function getRandomType() {
     return typeParams[Math.floor(Math.random() * typeParams.length)]
+}
+
+function getRandomPhrase() {
+    return genericPhrases[Math.floor(Math.random() * genericPhrases.length)]
 }
 
 function getComplement(type) {
@@ -25,5 +29,19 @@ function getComplement(type) {
 }
 
 const typeParams = ["dia", "tarde", "noite", "madrugada"]
+
+const genericPhrases = [
+    "A persistência é o caminho do êxito.",
+    "Que o vento leve o necessário e me traga o suficiente.",
+    "Algumas vezes coisas ruins acontecem em nossas vidas para nos colocar na direção das melhores coisas que poderíamos viver.",
+    "Ser feliz não é viver apenas momentos de alegria. É ter coragem de enfrentar os momentos de tristeza e sabedoria para transformar os problemas em aprendizado.",
+    "Se a caminhada está difícil, é porque você está no caminho certo.",
+    "Chique é ser feliz. Elegante é ser honesto. Bonito é ser caridoso. Sábio é saber ser grato. O resto é inversão de valores.",
+    "Ame seus pais, sua vida e seus amigos. Seus pais, porque são únicos. Sua vida, porque é curta demais. Seus amigos, porque são raros.",
+    "Seja humilde para admitir seus erros, inteligente para aprender com eles e maduro para corrigi-los.",
+    "Prefira o sorriso, faz bem a você e aos que estão ao seu redor. Dê risada de tudo, de si mesmo. Não adie alegrias. Seja feliz hoje!",
+    "O machado era de Assis. A rosa, do Guimarães. A bandeira, do Manuel. Mas feliz mesmo era o Jorge, que era amado.",
+    "Quando algo ruim acontece você tem três escolhas: deixar isso definir você, deixar isso destruir você ou fazer isso te deixar mais forte."
+]
 
 module.exports = getPhrase;
